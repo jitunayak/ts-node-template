1bd80ca9b49269f3faed38581d75a1a277e8ada7
jitunayak <jitunayak715@gmail.com>
2022-07-10 02:58:14 +0530

    use nodemon instead of ts-node

--- src/config.ts
+++ src/config.ts
@@ -10,32 +10,32 @@ const path = configureEnvPath(env)
 dotenv.config({ path: path })
 
 export const CONFIG = {
-    MONGO_URI: process.env.MONGO_URI,
-    ENV: process.env.NODE_ENV,
+        MONGO_URI: process.env.MONGO_URI,
+        ENV: process.env.NODE_ENV,
 }
 
 validateEnvironment()
 
 function configureEnvPath(env: string): string | undefined {
-    switch (env) {
-        case 'dev': {
-            const path = resolve(
-                __dirname,
-                `./../env/${process.env.NODE_ENV}.env`
-            )
-            return path
+        switch (env) {
+                case 'dev': {
+                        const path = resolve(
+                                __dirname,
+                                `./../env/${process.env.NODE_ENV}.env`
+                        )
+                        return path
+                }
+                case 'prod': {
+                        return undefined // prod deployment will have system enviroment values
+                }
+                default:
+                        throw new Error('No Envrionemt is specified')
         }
-        case 'prod': {
-            return undefined // prod deployment will have system enviroment values
-        }
-        default:
-            throw new Error('No Envrionemt is specified')
-    }
 }
 
 function validateEnvironment() {
-    Object.entries(CONFIG).map((key) => {
-        if (key[1] === undefined || key[1] === null)
-            throw new Error(`${MISSING_ENV} ${key[0]}`) // throw error if any enviroment values are missing
-    })
+        Object.entries(CONFIG).map((key) => {
+                if (key[1] === undefined || key[1] === null)
+                        throw new Error(`${MISSING_ENV} ${key[0]}`) // throw error if any enviroment values are missing
+        })
 }
